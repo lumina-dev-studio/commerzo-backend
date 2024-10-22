19fe18711b0a976ea45c36938bc9770057ee7b4f
Lumina Dev Studio <luminadevstudio@gmail.com>
2024-10-22 17:51:13 +0600

    trying

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,37 +1,37 @@
-import { NextFunction, Request, Response } from "express";
+// import { NextFunction, Request, Response } from "express";
 
-import catchAsync from "../../shared/catchAsync";
-import config from "../../config";
-import { jwtHelpers } from "../../helpers/jwtHelpers";
+// import catchAsync from "../../shared/catchAsync";
+// import config from "../../config";
+// import { jwtHelpers } from "../../helpers/jwtHelpers";
 
-type TRole = "Admin" | "User";
-const auth = (...requiredRoles: TRole[]) => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req?.headers?.authorization;
+// type TRole = "Admin" | "User";
+// const auth = (...requiredRoles: TRole[]) => {
+//   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+//     const token = req?.headers?.authorization;
 
-    console.log(req?.headers?.authorization)
+//     console.log(req?.headers?.authorization)
 
-    if (!token) {
-      throw new Error("Unauthorized Access");
-    }
+//     if (!token) {
+//       throw new Error("Unauthorized Access");
+//     }
 
-    let decoded;
+//     let decoded;
 
-    try {
-      decoded = jwtHelpers.verifyToken(token, config.jwt.jwt_secret as string);
-    } catch (err) {
-      throw new Error("Unauthorized Access");
-    }
+//     try {
+//       decoded = jwtHelpers.verifyToken(token, config.jwt.jwt_secret as string);
+//     } catch (err) {
+//       throw new Error("Unauthorized Access");
+//     }
 
-    const { role } = decoded;
+//     const { role } = decoded;
 
-    //  role checking
-    if (requiredRoles && !requiredRoles.includes(role)) {
-      throw new Error("Unauthorized Access");
-    }
+//     //  role checking
+//     if (requiredRoles && !requiredRoles.includes(role)) {
+//       throw new Error("Unauthorized Access");
+//     }
 
-    next();
-  });
-};
+//     next();
+//   });
+// };
 
-export default auth;
+// export default auth;
